23db2a74b2709bbf6df7e0b0faf54be69bdb18ea
kmiraie <kavonmiraie@yahoo.com>
2024-07-09 23:03:16 +0900

    centered the grid

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let container = document.querySelector('.container')
+let button = document.querySelector('.button');
 
 
 for(let i = 0; i<16; i++) {
@@ -6,12 +7,36 @@ for(let i = 0; i<16; i++) {
         let gridPiece = document.createElement('section')
         gridPiece.classList.add('grid-piece')
         container.appendChild(gridPiece)
+
+        gridPiece.addEventListener('mouseover', () => {
+            gridPiece.style.backgroundColor = 'blue';
+        })
     }
 }
 
 
 
+button.addEventListener('click', () => {
+    let userInput
+    do {
+    userInput = prompt('What size grid would you like? (less than 100)')
+    
+    } while(userInput > 100) 
+        container.innerHTML = ''
+    for(let i = 0; i<userInput; i++) {
+        for(let j = 0; j<userInput; j++) {
+            let gridPiece = document.createElement('section')
+            gridPiece.classList.add('grid-piece')
+            container.appendChild(gridPiece)
+    
+            colorChange(gridPiece)
+        
+        }
+    }
+})
 
-
-
-
+function colorChange(variable) {
+    variable.addEventListener('mouseover', () => {
+        variable.style.backgroundColor = 'blue';
+    })
+}
