8edd1c74724b0a591d22c2a7f8407cb15cd765e4
kmiraie <kavonmiraie@yahoo.com>
2024-07-11 15:08:59 +0900

    randomized the color

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let container = document.querySelector('.container')
 let button = document.querySelector('.button');
-
+let containerSize = 500;
 
 function createGrid(size) {
     container.innerHTML = ''
+    const gridPieceSize = (containerSize - (size -1)*2) / size;
     for(let i = 0; i<size*size; i++) {
         let gridPiece = document.createElement('section')
+        gridPiece.style.width = `${gridPieceSize}px`
+        gridPiece.style.height = `${gridPieceSize}px`
         gridPiece.classList.add('grid-piece')
         container.appendChild(gridPiece)
         
@@ -27,7 +30,11 @@ button.addEventListener('click', () => {
 
 function mouseOverHandler(event) {
     if(event.target.classList.contains('grid-piece')) {
-        event.target.style.backgroundColor = 'blue'
+        let randomBackgroundColor = `rgba(${Math.floor(Math.random()*256)}, 
+                                ${Math.floor(Math.random()*256)}, 
+                                ${Math.floor(Math.random()*256)}, 
+                                ${Math.random().toFixed(2)})`
+        event.target.style.backgroundColor = randomBackgroundColor
     }
 }
 
@@ -40,7 +47,7 @@ container.addEventListener('dragstart', (event) => {
 container.addEventListener('mousedown', (event) => {
     isMouseDown = true;
     if (event.target.classList.contains('grid-piece')) {
-        event.target.style.backgroundColor = 'blue';
+        mouseOverHandler(event);
         container.addEventListener('mouseover', mouseOverHandler);
     }
 })
